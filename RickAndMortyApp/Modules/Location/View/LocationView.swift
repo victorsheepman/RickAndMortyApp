db4e33af954ecfcebdac1ec668d9e51011a626f2
Victor Marquez <72099625+victorsheepman@users.noreply.github.com>
2024-07-21 12:44:23 -0400

    refactoring

--- RickAndMortyApp/Modules/Location/View/LocationView.swift
+++ RickAndMortyApp/Modules/Location/View/LocationView.swift
@@ -8,22 +8,23 @@
 import SwiftUI
 
 struct LocationView: View {
+    
     @StateObject var locationViewModel = LocationViewModel()
     
     @State private var isPresented: Bool   = false
     @State private var name: String        = ""
     @State private var type: String        = ""
     @State private var dimension: String   = ""
-    
-    let layout = [
+   
+    let gridLayout = [
         GridItem(.flexible(), spacing: 30),
         GridItem(.flexible(), spacing: 30)
     ]
-    
+
     var body: some View {
         NavigationView {
             ScrollView(.vertical, showsIndicators: false) {
-                LazyVGrid(columns: layout){
+                LazyVGrid(columns: gridLayout){
                     ForEach(locationViewModel.locations, id:\.id){ location in
                         NavigationLink(destination: LocationDetailView(locationId: location.id ?? 0)) {
                             LocationCard(type: location.type ?? "", name: location.name ?? "")
@@ -50,13 +51,17 @@ struct LocationView: View {
             }.background(Color("Gray7"))
             
         }
-        
         .onAppear{
-            
             locationViewModel.getLocations(from: "page=3")
         }
         .fullScreenCover(isPresented: $isPresented, onDismiss: { isPresented = false}){
-            FilterLocationView(name: $name, type: $type, dimension: $dimension, isPresented: $isPresented, manager: locationViewModel)
+            FilterLocationView(
+                name: $name, 
+                type: $type,
+                dimension: $dimension,
+                isPresented: $isPresented,
+                manager: locationViewModel
+            )
         }
     }
 }
