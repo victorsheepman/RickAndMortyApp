a933dc29b5b4ea3d1fc7d7abe1c8fec1ca17aaf6
Victor Marquez <72099625+victorsheepman@users.noreply.github.com>
2024-07-24 12:16:09 -0400

    progress

---
+++ RickAndMortyApp/Model/EpisodeResponseDataModel.swift
@@ -0,0 +1,57 @@
+//
+//  EpisodeResponseDataModel.swift
+//  RickAndMortyApp
+//
+//  Created by Victor Marquez on 24/7/24.
+//
+
+import Foundation
+
+// MARK: - EpisodeResponseDataModel
+struct EpisodeResponseDataModel: Codable {
+    let info: Info
+    let results: [EpisodeDataModel]
+}
+
+
+// MARK: - Result
+struct EpisodeDataModel: Codable {
+    let id: Int
+    let name, airDate, episode: String
+    let characters: [String]
+    let url: String
+    let created: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, name
+        case airDate = "air_date"
+        case episode, characters, url, created
+    }
+}
+
+// MARK: - Encode/decode helpers
+
+class JSONNull: Codable, Hashable {
+
+    public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
+        return true
+    }
+
+    public var hashValue: Int {
+        return 0
+    }
+
+    public init() {}
+
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if !container.decodeNil() {
+            throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encodeNil()
+    }
+}
