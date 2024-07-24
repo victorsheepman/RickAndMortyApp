b4f4923fd38a128ba67d29801ac582c17274dd52
Victor Marquez <72099625+victorsheepman@users.noreply.github.com>
2024-07-24 12:11:08 -0400

    progress

---
+++ RickAndMortyApp/Resource/iOSCheckboxToggleStyle.swift
@@ -0,0 +1,27 @@
+//
+//  iOSCheckboxToggleStyle.swift
+//  RickAndMortyApp
+//
+//  Created by Victor Marquez on 24/7/24.
+//
+
+
+import Foundation
+import SwiftUI
+
+struct iOSRadioButtonToggleStyle: ToggleStyle {
+    var onColor:  Color = Color("Indigo")
+    var offColor: Color = Color.gray1
+    
+    func makeBody(configuration: Configuration) -> some View {
+        Button(action: {
+            configuration.isOn.toggle()
+        }, label: {
+            HStack {
+                Image(systemName: configuration.isOn ? "largecircle.fill.circle" : "circle")
+                    .foregroundColor(configuration.isOn ? onColor : offColor)
+                configuration.label
+            }
+        })
+    }
+}
